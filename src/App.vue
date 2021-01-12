e3ec2cca1467c1c681ced8f71641cabc6b240b2f
bigheadc <18296278612@163.com>
2021-01-12 20:54:51 +0800

    修改h2-演示脚本

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
       <h2>预祝0713java全体学生前程似锦</h2>
       <h2>预祝0713java全体学生统统月薪过万</h2>
+      <h2>预祝0713java全体学生统统月薪过万</h2>
    <img src="http://bigheadc-gulionline-file.oss-cn-shanghai.aliyuncs.com/avatar/7f85bc8c3a0446749f42fd06e7d73923.jpg"/>
   </div>
 </template>
